e419f351dc3aa112c61a52a34b5918dca5aaa306
LoicBrassart <loic.brassart@gmail.com>
2019-10-26 11:59:41 +0200

    Added content to Footer.jsx and used the style file

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,62 @@
 import React from "react";
+import "./styles/Footer.scss";
 
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer id="Footer">
+      <nav>
+        <div>
+          <h3>Explorer</h3>
+          <ul>
+            <li>Accueil</li>
+            <li>Personnages</li>
+            <li>Scenarii</li>
+            <li>Groupes</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Liens utiles</h3>
+          <ul>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://scenariotheque.org"
+              >
+                Scénariothèque
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.aidedd.org/"
+              >
+                AideDD
+              </a>
+            </li>
+            <li>Cartes</li>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://roll20.net/"
+              >
+                Roll20
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h3>Divers</h3>
+          <ul>
+            <li>Nous contacter</li>
+            <li>Mentions légales</li>
+            <li>Contributeurs</li>
+          </ul>
+        </div>
+      </nav>
+      <p>Copyright 2019 - Tous droits réservés</p>
+    </footer>
+  );
 }
