d0daf9bd1994960f974cf014c8df3f4911702e18
LoicBrassart <loic.brassart@gmail.com>
2019-10-26 11:07:25 +0200

    Added several basic layout components

---
+++ frontend/src/components/MetaBar.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MetaBar() {
+  return <header>MetaBar</header>;
+}
---
+++ frontend/src/components/RoleetBar.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function RoleetBar() {
+  return <header>RoleetBar</header>;
+}
