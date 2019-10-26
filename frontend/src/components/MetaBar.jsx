4b48a4ed23901bc6eedf2238dbace78e624693a9
LoicBrassart <loic.brassart@gmail.com>
2019-10-26 12:01:45 +0200

    Added SCSS files for basic layout elements, linked them to JSX files

--- frontend/src/components/MetaBar.jsx
+++ frontend/src/components/MetaBar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./styles/MetaBar.scss";
 
 export default function MetaBar() {
   return <header>MetaBar</header>;
--- frontend/src/components/RoleetBar.jsx
+++ frontend/src/components/RoleetBar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./styles/RoleetBar.scss";
 
 export default function RoleetBar() {
   return <header>RoleetBar</header>;
