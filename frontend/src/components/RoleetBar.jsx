ef6430cae450ad1d59e363b7418057a66af2dc4b
LoicBrassart <loic.brassart@gmail.com>
2019-10-27 10:51:12 +0100

    Added basic suppport for Roleetbar

--- frontend/src/components/RoleetBar.jsx
+++ frontend/src/components/RoleetBar.jsx
@@ -2,5 +2,24 @@ import React from "react";
 import "./styles/RoleetBar.scss";
 
 export default function RoleetBar() {
-  return <header>RoleetBar</header>;
+  return (
+    <nav id="RoleetBar">
+      <div className="logo center">
+        <div>
+          <img src="img/logo.svg" alt="" />
+          <h1>Roleet</h1>
+        </div>
+        <p>May the Dice be with you !</p>
+      </div>
+      <ul className="flexer">
+        <li className="center">SearchOverWebsite</li>
+        <li className="center">Personnages</li>
+        <li className="center">Scenarii</li>
+        <li className="center">Groupes</li>
+        <li className="center">
+          <img src="img/user-avatar-default.png" alt="Mon compte Roleet" />
+        </li>
+      </ul>
+    </nav>
+  );
 }
