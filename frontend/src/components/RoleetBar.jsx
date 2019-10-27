8a27d82c655b1adbaa88d5b946f0ddac72a232c6
LoicBrassart <loic.brassart@gmail.com>
2019-10-27 21:30:03 +0100

    Added links to the main menu

--- frontend/src/components/RoleetBar.jsx
+++ frontend/src/components/RoleetBar.jsx
@@ -1,21 +1,32 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 import "./styles/RoleetBar.scss";
 
 export default function RoleetBar() {
   return (
     <nav id="RoleetBar">
       <div className="logo center">
-        <div>
-          <img src="img/logo.svg" alt="" />
-          <h1>Roleet</h1>
-        </div>
-        <p>May the Dice be with you !</p>
+        <Link to="/">
+          <div>
+            <img src="img/logo.svg" alt="" />
+            <h1>Roleet</h1>
+          </div>
+          <p>May the Dice be with you !</p>
+        </Link>
       </div>
       <ul className="flexer">
         <li className="center">SearchOverWebsite</li>
-        <li className="center">Personnages</li>
-        <li className="center">Scenarii</li>
-        <li className="center">Groupes</li>
+        <li className="center">
+          <NavLink to="/characters" activeClassName="active">
+            Personnages
+          </NavLink>
+        </li>
+        <li className="center">
+          <NavLink to="/scenarii">Scenarii</NavLink>
+        </li>
+        <li className="center">
+          <NavLink to="/groups">Groupes</NavLink>
+        </li>
         <li className="center">
           <img src="img/user-avatar-default.png" alt="Mon compte Roleet" />
         </li>
