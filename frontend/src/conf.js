69ee29189a1dfe96d4bdcae05c974803b68c8e05
LoicBrassart <loic.brassart@gmail.com>
2019-10-31 07:41:38 +0100

    (Minor typo) Updated  to latest version

--- frontend/src/conf.js
+++ frontend/src/conf.js
@@ -1,7 +1,8 @@
-const axios = require("axios");
+//const axios = require("axios");
 require("dotenv").config();
-
+/*
 const api = axios.create({
   baseURL: process.env.REACT_APP_APIHOST || "http://localhost:4040/"
 });
 module.exports = { api };
+*/
