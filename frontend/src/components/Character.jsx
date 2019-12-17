b9a7b25eb97ec184ab16baf48ead59a7bd5fe32e
LoicBrassart <loic.brassart@gmail.com>
2019-12-17 16:03:09 +0100

    Added first pass, characters are displayed even though everything is broken

--- frontend/src/components/Character.jsx
+++ frontend/src/components/Character.jsx
@@ -1,5 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./styles/Character.scss";
 
-export default function Character({ name }) {
-  return <div>{name}</div>;
+const FluffsTab = ({ fluffs }) => (
+  <article className="descriptionArticle articleSelected">
+    {fluffs.map((fluff, i) => {
+      return (
+        <React.Fragment key={i}>
+          <h3>{fluff.title}</h3>
+          <p>{fluff.content}</p>
+        </React.Fragment>
+      );
+    })}
+  </article>
+);
+
+const SheetsTab = ({ sheets }) => (
+  <article className="sheetsArticle articleSelected">
+    <ul>
+      {sheets.map((sheet, i) => {
+        return (
+          <li key={i}>
+            <Link to="/">{sheet.gameSystem}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  </article>
+);
+
+export default function Character({
+  avatar,
+  name,
+  baseline,
+  tags,
+  fluffs,
+  characterSheets
+}) {
+  const [currentTab, changeTab] = useState("fluffs");
+
+  return (
+    <section className="Character">
+      <img src={avatar} alt={name} />
+      <article>
+        <h2>{name}</h2>
+        <p>{baseline}</p>
+        <aside>
+          {tags.map((tag, i) => {
+            return <span key={i}>{tag}</span>;
+          })}
+        </aside>
+      </article>
+
+      <aside>
+        <nav>
+          <button
+            type="button"
+            className={`${
+              currentTab === "fluffs" ? "buttonSelected" : ""
+            } descriptionButton cardButton`}
+            onClick={() => {
+              changeTab("fluffs");
+            }}
+          >
+            Description
+          </button>
+          <button
+            type="button"
+            className={`${
+              currentTab === "sheets" ? "buttonSelected" : ""
+            } sheetsButton cardButton`}
+            onClick={() => {
+              changeTab("sheets");
+            }}
+          >
+            Sheets
+          </button>
+        </nav>
+
+        {currentTab === "fluffs" ? (
+          <FluffsTab fluffs={fluffs} />
+        ) : (
+          <SheetsTab sheets={characterSheets} />
+        )}
+      </aside>
+    </section>
+  );
 }
--- frontend/src/components/Characters.jsx
+++ frontend/src/components/Characters.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Character from "./Character";
 
 export default function Characters({ data }) {
-  return data.map(char => {
-    return <Character {...char} />;
+  return data.map((char, i) => {
+    return <Character key={i} {...char} />;
   });
 }
