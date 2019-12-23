5a78be68df50cb1db0aef26d32d2e2b4ff8d7a36
LoicBrassart <loic.brassart@gmail.com>
2019-12-23 14:43:25 +0100

    Added the Badge component

---
+++ frontend/src/components/Badge.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./styles/Badge.scss";
+
+export default function Badge({ linkTo, imgPath, imgAlt }) {
+  return (
+    <li className="Badge wip">
+      <Link to={linkTo}>
+        <img src={imgPath} alt={imgAlt} />
+      </Link>
+    </li>
+  );
+}
