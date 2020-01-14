64b2d852884df1169c80afca54802bc9fe14b720
LoicBrassart <loic.brassart@gmail.com>
2020-01-14 22:24:29 +0100

    Added a connection form

---
+++ frontend/src/forms/ConnectionForm.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Formik, Form } from "formik";
+import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import { api } from "../conf";
+import { TextType, PasswordType } from "./fields/index";
+
+export default function ConnectionForm() {
+  const dispatch = useDispatch();
+  return (
+    <Formik
+      initialValues={{
+        pseudo: "",
+        password: ""
+      }}
+      onSubmit={values => {
+        api
+          .post("/users", values)
+          .then(({ data }) => {
+            dispatch({ type: "LOGIN_MOCK_USER" });
+          })
+          .catch(() => {
+            toast.error(
+              "We encountered an error while logging in, sorry about that"
+            );
+          });
+      }}
+    >
+      <Form>
+        <TextType label="Pseudo" name="pseudo" placeholder="Jane" />
+        <PasswordType label="Mot de passe" name="password" placeholder="Jane" />
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
+  );
+}
