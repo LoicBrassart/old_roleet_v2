009fda6d6920264c873e0648261d9c8ff5d72e30
LoicBrassart <loic.brassart@gmail.com>
2020-01-24 20:10:34 +0100

    Added support for fluffs (I'm *really* not fond of this component, I'll have to try and do better than that)

--- frontend/src/forms/CharacterForm.jsx
+++ frontend/src/forms/CharacterForm.jsx
@@ -8,10 +8,23 @@ import "@pathofdev/react-tag-input/build/index.css";
 const schema = yup.object().shape({});
 
 export function CharacterForm({ cb }) {
+  const [fluffs, setFluffs] = useState([]);
   const [tags, setTags] = useState([]);
+  const [counterFluffs, setCounterFluffs] = useState(0);
+
   const { register, handleSubmit, errors } = useForm({
     validationSchema: schema
   });
+
+  const addFluff = () => {
+    setFluffs(prevFluffs => [...prevFluffs, counterFluffs]);
+    setCounterFluffs(prevCounterFluffs => prevCounterFluffs + 1);
+  };
+
+  const removeFluff = index => () => {
+    setFluffs(prevFluffs => [...prevFluffs.filter(item => item !== index)]);
+    setCounterFluffs(prevCounterFluffs => prevCounterFluffs - 1);
+  };
 
   const onSubmit = data => {
     data.tags = data.tags.split(",");
@@ -22,50 +35,80 @@ export function CharacterForm({ cb }) {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <input type="hidden" name="author" value="42" ref={register} />
+      <>
+        <label htmlFor="name" hidden>
+          Nom
+        </label>
+        <input
+          type="text"
+          ref={register}
+          name="name"
+          placeholder="Sylvanas Murmevent"
+        />
+        {errors.name && <p>{errors.name.message}</p>}
+      </>
 
-      <label htmlFor="name" hidden>
-        Nom
-      </label>
-      <input
-        type="text"
-        ref={register}
-        name="name"
-        placeholder="Sylvanas Murmevent"
-      />
-      {errors.name && <p>{errors.name.message}</p>}
+      <>
+        <label htmlFor="baseline" hidden>
+          Citation
+        </label>
+        <input
+          type="text"
+          ref={register}
+          name="baseline"
+          placeholder="No turning back !"
+        />
+        {errors.baseline && <p>{errors.baseline.message}</p>}
+      </>
 
-      <label htmlFor="baseline" hidden>
-        Citation
-      </label>
-      <input
-        type="text"
-        ref={register}
-        name="baseline"
-        placeholder="No turning back !"
-      />
-      {errors.baseline && <p>{errors.baseline.message}</p>}
+      <>
+        <ReactTagInput
+          tags={tags}
+          ref={register}
+          onChange={newTags => setTags(newTags)}
+          placeholder="Horde, Undead, ..."
+          removeOnBackspace
+          validator={value => {
+            const hasComma = value.indexOf(",") !== -1;
+            if (hasComma)
+              toast.error("Pas de virgule dans les tags s'il te plait !");
+            return !hasComma;
+          }}
+        />
+        <input type="hidden" name="tags" value={tags} ref={register} />
+      </>
 
-      <ReactTagInput
-        tags={tags}
-        ref={register}
-        onChange={newTags => setTags(newTags)}
-        placeholder="Horde, Undead, ..."
-        removeOnBackspace
-        validator={value => {
-          const hasComma = value.indexOf(",") !== -1;
-          if (hasComma)
-            toast.error("Pas de virgule dans les tags s'il te plait !");
-          return !hasComma;
-        }}
-      />
-      <input type="hidden" name="tags" value={tags} ref={register} />
+      <>
+        {fluffs.map(index => {
+          const fieldName = `fluffs[${index}]`;
+          return (
+            <fieldset name={fieldName} key={index}>
+              <label>
+                Titre:
+                <input type="text" name={`${fieldName}.title`} ref={register} />
+              </label>
 
+              <label>
+                Contenu:
+                <input
+                  type="text"
+                  name={`${fieldName}.content`}
+                  ref={register}
+                />
+              </label>
+              <button type="button" onClick={removeFluff(index)}>
+                Supprimer
+              </button>
+            </fieldset>
+          );
+        })}
+        <button type="button" onClick={addFluff}>
+          Ajouter
+        </button>
+      </>
       {/* Avatar: file */}
-      {/* Fluffs: array */}
-      {/*   Title: text */}
-      {/*   Content: text */}
 
-      <input type="submit" />
+      <input type="submit" value="Sauvegarder" />
     </form>
   );
 }
