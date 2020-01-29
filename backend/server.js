92fd3ca89a0b861e19f6871f7b459a3b21804345
LoicBrassart <loic.brassart@gmail.com>
2020-01-29 23:42:00 +0100

    Added (very) basic logging for every request

--- backend/server.js
+++ backend/server.js
@@ -4,20 +4,31 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const passport = require("passport");
 const { backendPort } = require("./conf");
+const pino = require("pino");
+const logger = pino({ level: process.env.LOG_LEVEL || "info" });
+const expressPino = require("express-pino-logger");
+const expressLogger = expressPino({ logger });
 
 /* ------------------------------------------------------------- Tools */
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(passport.initialize());
+app.use(expressLogger);
+
+app.use((req, res, next) => {
+  logger.debug(`Request for: ${req.url}`);
+  next();
+});
+/* ------------------------------------------------------------- Routes */
 
 /* ------------------------------------------------------------- 404 and server launch */
 app.use((req, res, next) => {
   const msg = `Page not found: ${req.url}`;
-  console.error(msg);
+  logger.debug(msg);
   res.status(404).send(msg);
 });
 
 app.listen(backendPort, () => {
-  console.log(`API root available at: http://localhost:${backendPort}/`);
+  logger.debug(`API root available at: http://localhost:${backendPort}/`);
 });
