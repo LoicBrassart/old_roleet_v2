84d1f444fb9a35550d04907897f2b565052c1481
LoicBrassart <loic.brassart@gmail.com>
2020-02-04 14:16:15 +0100

    Added a fixtures loader script (WiP)

---
+++ backend/scripts/loadDatabase.js
@@ -0,0 +1,45 @@
+const { db } = require("../conf");
+const { Character, User, Scenario } = require("../models");
+let data;
+
+data = require("../../fixtures/users.json");
+db.dropCollection("users");
+data.map(userData => {
+  const newUser = new User(userData);
+  newUser.save(err => {
+    if (err) {
+      console.error(`Failure for User ${userData.pseudo}: ${err}`);
+      return;
+    }
+    console.log(`User recorded: ${userData.pseudo}`);
+  });
+  return;
+});
+
+data = require("../../fixtures/characters.json");
+db.dropCollection("characters");
+data.map(charData => {
+  const newChar = new Character(charData);
+  newChar.save(err => {
+    if (err) {
+      console.error(`Failure for Character ${charData.name}: ${err}`);
+      return;
+    }
+    console.log(`Character recorded: ${charData.name}`);
+  });
+  return;
+});
+
+data = require("../../fixtures/scenarii.json");
+db.dropCollection("scenarii");
+data.map(scenData => {
+  const newScen = new Scenario(scenData);
+  newScen.save(err => {
+    if (err) {
+      console.error(`Failure for Scenario ${scenData.title}: ${err}`);
+      return;
+    }
+    console.log(`Scenario recorded: ${scenData.title}`);
+  });
+  return;
+});
